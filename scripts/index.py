3f629dbd3423ce0b843c67377bf5afdd9f26fc01
corranmac <corranmac@hotmail.co.uk>
2023-07-25 02:40:53 +0100

    Update index.py

--- scripts/index.py
+++ scripts/index.py
@@ -5,15 +5,15 @@ import pandas as pd
 import pyarrow.parquet as pq
 
 class Faiss_Index:
-    def __init__(self, shape=None, db_name="my_documents", index_dir='index'):
-        index_path = os.path.join(self.index_dir, self.index_name)
-      
+    def __init__(self, shape=None, db_name="my_documents", db_dir='db'):
+        index_dir =  os.path.join(db_dir, db_name)
+        index_path = os.path.join(index_dir, "/index.index")
         # Check if index already exists
         if os.path.exists(index_path):
-            self.index = faiss.read_index(index_path)
+            index = faiss.read_index(index_path)
         else:
-            index = faiss.IndexFlatL2(shape)
-            index = faiss.IndexIDMap(index_path) 
+            os.makedirs(index_dir,exist_ok=True)
+            index = faiss.IndexFlatL2(384)
             faiss.write_index(index, index_path)
         self.index_path = index_path
         self.index = index
@@ -42,21 +42,22 @@ class Faiss_Index:
     def get_embeddings(self, indexes):
         return self.index.reconstruct_n(indexes)
 
+
 class MetaStore:
-    def __init__(self, db_name="my_documents", metadata_dir="metadata", columns = ['file_path', 'file_id', 'chunk_id', 'chunk_text', 'is_indexed', 'index_id', 'custom_metadata']):
-        metadata_path = os.path.join(metadata_dir, db_name)
-        parquet_path = os.path.join(metadata_path, 'metadata.parquet')
+    def __init__(self, db_name="my_documents", db_dir="metadata", columns = ['file_path', 'file_id', 'chunk_id', 'chunk_text', 'is_indexed', 'index_id', 'custom_metadata']):
+        index_dir =  os.path.join(db_dir, db_name)
+        parquet_path = os.path.join(index_dir, 'metadata.parquet')
         
         if os.path.exists(parquet_path):
             # Load existing Parquet file
             df = pd.read_parquet(parquet_path)
         else:
             # Create new Parquet file
-            df = pd.DataFrame(columns)
+            os.makedirs(index_dir,exist_ok=True)
+            df = pd.DataFrame(columns=columns)
             df.to_parquet(parquet_path)
 
         self.parquet_path = parquet_path
-        self.metadata_path = metadata_path
         self.df = df
 
     def add_chunk(self, chunk_dicts):
