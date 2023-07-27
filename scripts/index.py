8631b421f53f87128c77f767e884a67a143b5be0
corranmac <corranmac@hotmail.co.uk>
2023-07-27 16:16:45 +0100

    Update index.py

--- scripts/index.py
+++ scripts/index.py
@@ -5,9 +5,9 @@ import pandas as pd
 import pyarrow.parquet as pq
 
 class Faiss_Index:
-    def __init__(self, shape=None, db_name="my_documents", db_dir='db'):
-        index_dir =  os.path.join(db_dir, db_name)
-        index_path = os.path.join(index_dir, "/index.index")
+    def __init__(self, shape=None, collection_name="my_documents", db_dir='db'):
+        index_dir =  os.path.join(".",db_dir, collection_name)
+        index_path = os.path.join(index_dir, "index.index")
         # Check if index already exists
         if os.path.exists(index_path):
             index = faiss.read_index(index_path)
@@ -17,22 +17,23 @@ class Faiss_Index:
             faiss.write_index(index, index_path)
         self.index_path = index_path
         self.index = index
+        self.collection_name = collection_name
     
     def search(self, query, k):
-        return self.index.search(query, k)
+        return self.index.search(np.array(query), k)
 
     def load(self,index_path):
         # Load index from disk
         self.index = faiss.read_index(index_path)
 
-    def add(self, embeddings, chunk_ids):
+    def add(self, embeddings, chunk_ids,meta_store):
         self.index.add(embeddings)
-        self.index.save()
+        faiss.write_index(self.index, self.index_path)
 
         #Get indexes of recently added chunks
-        start_index = index.ntotal - num_added
-        index_list = list(range(start_index, index.ntotal))
-        MetaStore.mark_indexed(chunk_ids,index_list)
+        start_index = self.index.ntotal - len(embeddings)
+        index_list = list(range(start_index, self.index.ntotal))
+        meta_store.mark_indexed(chunk_ids,index_list)
 
     def reset(self):
         # Reset and reload index
@@ -44,10 +45,10 @@ class Faiss_Index:
 
 
 class MetaStore:
-    def __init__(self, db_name="my_documents", db_dir="metadata", columns = ['file_path', 'file_id', 'chunk_id', 'chunk_text', 'is_indexed', 'index_id', 'custom_metadata']):
-        index_dir =  os.path.join(db_dir, db_name)
+    def __init__(self, collection_name="my_documents", db_dir="db", columns = []):
+        index_dir =  os.path.join(".",db_dir, collection_name)
         parquet_path = os.path.join(index_dir, 'metadata.parquet')
-        
+
         if os.path.exists(parquet_path):
             # Load existing Parquet file
             df = pd.read_parquet(parquet_path)
@@ -55,23 +56,31 @@ class MetaStore:
             # Create new Parquet file
             os.makedirs(index_dir,exist_ok=True)
             df = pd.DataFrame(columns=columns)
+
             df.to_parquet(parquet_path)
 
         self.parquet_path = parquet_path
         self.df = df
+        self.collection_name = collection_name
 
     def add_chunk(self, chunk_dicts):
-        self.df = self.df.append(chunk_dicts, ignore_index=True)
+        df = self.df
+        #Assign columns if MetaData is empty
+        if len(df) == 0: 
+          df = pd.DataFrame(list(columns=chunk_dicts[0].keys())+['is_indexed','index_id'])
+        #Append new metadata
+        self.df = df.append(chunk_dicts, ignore_index=False)
         self.save()
         
-    def mark_indexed(self, chunk_ids, index_ids):
+    def mark_indexed(self, chunk_ids,index_ids):
+        df = self.df
         self.df.loc[chunk_ids, 'is_indexed'] = True
         self.df.loc[chunk_ids, 'index_id'] = index_ids
         self.save()
 
     def get_rows_by_chunk_id(self, chunk_ids):
         """Fetch rows by list of chunk ids"""
-        return self.df.loc[self.df['chunk_id'].isin(chunk_ids)]
+        return self.df.loc[self.df['index_id'].isin(chunk_ids)]
 
     def save(self):
         # Save DataFrame back to Parquet
